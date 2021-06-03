5028cfca5cc0d7761f0ae9b03e980983b3456aee
thinkdoyan <tismychoice@gmail.com>
2021-06-03 11:45:31 +0530

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,10 +22,52 @@ exports.createPages = async (props) => {
     }
   `)
 
+
   const perPage = wpSettings.wp.readingSettings.postsPerPage || 10
   const blogURI = "/"
   const templates = getTemplates()
 
+  
+  
+ import React from "react"
+import { graphql } from "gatsby"
+
+const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+
+export const query = graphql`
+  {
+    pages {
+      edges {
+        node {
+          isFrontPage
+          isPostsPage
+          isPrivacyPage
+          isRestricted
+        }
+      }
+    }
+    menu(id: "") {
+      name
+    }
+    menuItem(id: "") {
+      label
+    }
+    writingSettings {
+      defaultCategory
+      defaultPostFormat
+      useSmilies
+    }
+    post(id: "1") {
+      id
+    }
+  }
+`
+
+export default ComponentName
+ 
+  
+  
+  
   await createContentTypes(props, { templates })
   await createBlog(props, { perPage, blogURI })
   await createCategories(props, { perPage })
