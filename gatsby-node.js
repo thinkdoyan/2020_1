241af5b34211138ec6df0d1288ced7f37a75b006
thinkdoyan <tismychoice@gmail.com>
2021-06-03 11:11:38 +0530

    WP Graph QL code
    Code added from website

--- gatsby-node.js
+++ gatsby-node.js
@@ -65,3 +65,41 @@ exports.createResolvers = async ({
     },
   })
 }
+
+
+
+import React from "react"
+import { graphql } from "gatsby"
+
+const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+
+export const query = graphql`
+  {
+    pages {
+      edges {
+        node {
+          isFrontPage
+          isPostsPage
+          isPrivacyPage
+          isRestricted
+        }
+      }
+    }
+    menu(id: "") {
+      name
+    }
+    menuItem(id: "") {
+      label
+    }
+    writingSettings {
+      defaultCategory
+      defaultPostFormat
+      useSmilies
+    }
+    post(id: "1") {
+      id
+    }
+  }
+`
+
+export default ComponentName
